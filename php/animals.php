f3bc9d05f970a9cafc0591df09bf8528c787c5bc
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-02 14:56:57 +0100

    feat: Add script to display total animal count from database.

---
+++ php/animals.php
@@ -0,0 +1,12 @@
+
+
+<?php
+    include("./php/connexion.php");
+
+
+    // total animals
+    $query_total = "SELECT * FROM animal";
+    $total = mysqli_query($conn,$query_total);
+    $total = mysqli_num_rows($total);
+
+?>
