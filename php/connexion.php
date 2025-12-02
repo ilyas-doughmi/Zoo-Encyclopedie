893773d090ba600f35e96e4a0d5ba23b7a6eda25
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-02 12:54:13 +0100

    feat: adding isConnected to connexion.php

--- php/connexion.php
+++ php/connexion.php
@@ -3,10 +3,16 @@ $server_name = "localhost";
 $user_name = "root";
 $password = "";
 $db_name = "zoo_encycloedie";
+$isConnected = false;
 
 try {
     $conn = mysqli_connect($server_name, $user_name, $password, $db_name);
+    $isConnected = true;
 } catch (mysqli_sql_exception) {
     echo "connexion failed";
+    $isConnected = false;
 }
+
+
+
 ?>
