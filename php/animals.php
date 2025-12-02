f637b515f65a45bf5b41ad372889244bf36914df
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-02 16:52:31 +0100

    feat: adding new variable called query_for_all to query for all data we have in animals

--- php/animals.php
+++ php/animals.php
@@ -7,6 +7,7 @@ include("connexion.php");
 // total animals
 if ($conn) {
     $query_total = "SELECT * FROM animal";
+    $query_for_all = mysqli_query($conn, $query_total);
     $total = mysqli_query($conn, $query_total);
     $total = mysqli_num_rows($total);
 
