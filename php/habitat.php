41ca7545dd845e15600abb8db51f6bd945ccaaab
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-04 14:48:56 +0100

    feat: adding edit & delete to habit.php

--- php/habitat.php
+++ php/habitat.php
@@ -52,6 +52,59 @@
             header("location: ../index.php?added=success");
         }
     }
+
+    // delete Habitat
+
+    if (isset($_GET["del_id"])) {
+        $id = $_GET["del_id"];
+        $delete = mysqli_query($conn, "DELETE FROM habitat WHERE id_hab=" . $id);
+
+        if (!$delete) {
+            echo "problem";
+        } else {
+            header("location: ../index.php?isDeleted = success");
+        }
+    }
+
+    // query with id 
+    function habitat_data_id($id)
+    {
+        global $conn;
+
+        $data_query = "SELECT * FROM habitat WHERE id_hab = " . $id . ";";
+        $data_query = mysqli_query($conn, $data_query);
+
+        if ($data_query) {
+            $data = mysqli_fetch_assoc($data_query);
+
+            echo json_encode($data);
+        } else {
+            echo "no data found";
+        }
+    }
+
+    // edit a habitat 
+    if (isset($_GET["data_id"])) {
+        $id = $_GET['data_id'];
+        habitat_data_id($id);
+        exit;
+    }
+
+    if (isset($_GET["id_edit_hab"])) {
+        $id = $_GET["id_edit_hab"];
+        $new_name = $_GET['edit_name_hab'];
+        $new_desc = $_GET["edit_desc_hab"];
+        
+        $query_edit = "UPDATE habitat SET name_hab = '".$new_name."',desc_hab = '".$new_desc."' WHERE id_hab = ".$id;
+
+        if(!mysqli_query($conn,$query_edit)){
+            echo "problem";
+        }
+        else{
+                header("Location: ../index.php");
+                exit;
+        }
+    }   
     
 
 ?>
